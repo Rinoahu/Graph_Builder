229c99158c5cc9e4a388d21fd07e540df25f7fe0
Rinoahu <xiaohu@iastate.edu>
2018-07-19 15:32:11 -0500

    add all the algorithm

--- rna_seq/naive_cluster.py
+++ rna_seq/naive_cluster.py
@@ -37,26 +37,46 @@ def main():
     except:
         thres = .2
 
+    _o = open(qry+'.npy', 'wb')
     f = open(qry, 'r')
+    f.next()
     n2s = {}
-    data = []
+    #data = []
     #data_row = []
-    flag = 0
+    R = 0
+    buf = []
     for i in f:
         h, j = get_row(i, 16)
         j = np.asarray(j, 'float32')
         #data_row.append(j)
         j = normal(j)
         j = np.nan_to_num(j)
-        data.append(j)
-        n2s[flag] = h
-        flag += 1
+        #data.append(j)
+        C = j.size
+        buf.append(j)
+        n2s[R] = h
+        R += 1
         #if flag > 10000:
         #    break
+        #if R > 10000:
+        #    break
+        if len(buf) > 1000:
+            buf = np.asarray(buf, 'float32')
+            _o.write(buf.data[:])
+            buf = []
 
-    data = np.asarray(data)
+    if buf:
+        buf = np.asarray(buf, 'float32')
+        _o.write(buf.data[:])
+        buf = []
+
+
+    #data = np.asarray(data)
     f.close()
+    _o.close()
 
+    data = np.memmap(qry+'.npy', mode='r', shape=(R, C), dtype='float32')
+    data = np.asarray(data, 'float32')
     N, D = data.shape
     #p = hnswlib.Index(space='ip', dim=D)
     #p.init_index(max_elements=N, ef_construction=100, M=16)
@@ -68,14 +88,17 @@ def main():
         # caculate pearson cor
         prs = np.dot(data, data[i:i + chk].T)
         rows, cols = np.where(prs >= thres)
-        cols += i
+        #cols += i
         for j in xrange(rows.size):
             r, c = rows[j], cols[j]
-            rs, cs = map(n2s.get, [r, c])
-            print rs, cs, prs[r, c]
+            rs, cs = map(n2s.get, [r, c+i])
+            output = [cs, rs, prs[r, c]]
+            print '\t'.join(map(str, output))
+            #print cs, rs, c, r, prs[r, c]
+            #print rs, cs, r, c, prs.shape
             #print rs, cs, prs[r, c], pearsonr(data_row[r], data_row[c])[0]
 
-
+    #print 'finish', i, r, c
 
 if __name__ == '__main__':
     main()
